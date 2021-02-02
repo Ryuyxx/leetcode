a0f8656dec4a9852fe917bbfa2e345f24af0a155
Ryuyxx <ryuyxx@gmail.com>
2021-02-03 00:03:55 +0900

    clean

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -4,7 +4,7 @@
 # x = -123
 # -321
 
-# x = 2200
+# x = 210
 # 21
 
 # x = 0
@@ -13,34 +13,56 @@
 # x = -10
 # -1
 
-x = 1563847412
+x = 1534236469
 # 0
 
-if x == 0: return 0
 
-lst=[]
-flag = False
-for i in str(x):
-    if i == "-":
-        flag = True
-        continue
-    lst.append(int(i))
+# lst=[]
+# flag = False
+# for i in str(x):
+#     if i == "-":
+#         flag = True
+#         continue
+#     lst.append(int(i))
+#
+# lst = lst[::-1]
+# lst = str(lst).translate(str.maketrans({'[': '', ']': '', ',':'', ' ':''}))
+# if flag == False:
+#     return 0 if -2147483648 <= abs(int(lst)) >= (1<<31)-1 else int(lst)
+# elif lst[0] == "0" and flag == True:
+#     while lst[0] == "0":
+#         lst = lst[1:]
+#     lst = "-" + lst
+#     return 0 if -2147483648 <= abs(int(lst)) >= (1<<31)-1 else int(lst)
+# elif lst[0] == "0":
+#     while lst[0] == "0":
+#         lst = lst[1:]
+#     return 0 if -2147483648 <= abs(int(lst)) >= (1<<31)-1 else int(lst)
+# else:
+#     lst = "-" + lst
+#     return 0 if -2147483648 <= abs(int(lst)) >= (1<<31)-1 else int(lst)
 
-lst = lst[::-1]
-lst = str(lst).translate(str.maketrans({'[': '', ']': '', ',':'', ' ':''}))
-if flag == False:
-    # if -2147483648 <= abs(int(lst)) >= (1<<31)-1 : lst = 0
-    # return int(lst)
-    return 0 if -2147483648 <= abs(int(lst)) >= (1<<31)-1 else int(lst)
-elif lst[0] == "0" and flag == True:
-    while lst[0] == "0":
-        lst = lst[1:]
-    lst = "-" + lst
-    return 0 if -2147483648 <= abs(int(lst)) >= (1<<31)-1 else int(lst)
-elif lst[0] == "0":
-    while lst[0] == "0":
-        lst = lst[1:]
-    return 0 if -2147483648 <= abs(int(lst)) >= (1<<31)-1 else int(lst)
+
+#amelioration
+
+
+## submit
+
+if 2**31 <= x or x <= -2**31-1 : return 0
+if x >= 0:
+    ans = int(str(x) [::-1])
+    return 0 if 2**31 <= ans or ans <= -2**31-1 else ans
 else:
-    lst = "-" + lst
-    return 0 if -2147483648 <= abs(int(lst)) >= (1<<31)-1 else int(lst)
+    ans = int("-"+str(x)[1:][::-1])
+    return 0 if 2**31 <= ans or ans <= -2**31-1 else ans
+
+
+## local test
+
+if 2**31 <= x or x <= -2**31-1 : print(0)
+if x >= 0:
+    ans = int(str(x) [::-1])
+    print(0) if 2**31 <= ans or ans <= -2**31-1 else print(ans)
+else:
+    ans = int("-"+str(x)[1:][::-1])
+    print(0) if 2**31 <= ans or ans <= -2**31-1 else print(ans)
