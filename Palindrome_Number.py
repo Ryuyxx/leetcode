4a465a4c5f2c280f18d44f155297ed9e4ec9df89
Ryuyxx <ryuyxx@gmail.com>
2021-02-03 10:29:58 +0900

    add

---
+++ Palindrome_Number.py
@@ -0,0 +1,17 @@
+x = 121
+# true
+#
+# x = -121
+# false
+# 
+# x = 10
+# false
+#
+# x = -101
+# false
+#
+# x = 2147483648
+# x = -2147483650
+
+# if not -2**31-1 <= x <= 2**31-1 : print(0)
+return list(str(x)) == list(str(x))[::-1]
