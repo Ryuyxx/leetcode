7b263387d378a1444a2e282af248f8becdaf300d
Ryuyxx <Ryuyxx@gmail.com>
2021-02-12 23:45:12 +0900

    abandon

---
+++ Problems/38.py
@@ -0,0 +1,14 @@
+n = 1
+# Output: "1"
+
+n = 4
+# Output: "1211"
+# Explanation:
+# countAndSay(1) = "1"
+# countAndSay(2) = say "1" = one 1 = "11"
+# countAndSay(3) = say "11" = two 1's = "21"
+# countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
