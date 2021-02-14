5b0065720610986b869833f2498dacaa53ab60d7
Ryuyxx <Ryuyxx@gmail.com>
2021-02-14 19:10:20 +0900

    add: template

---
+++ Problems/template.py
@@ -0,0 +1,36 @@
+"""
+2021/00/00
+
+[Solved][Attempted]
+
+"""
+
+import unittest
+
+
+class Solution:
+
+
+
+
+class Test(unittest.TestCase):
+    def test1(self):
+        i =
+        e =
+        self.assertEqual(e, Solution().sample(i))
+    def test2(self):
+        i =
+        e =
+        self.assertEqual(e, Solution().sample(i))
+    def test3(self):
+        i =
+        e =
+        self.assertEqual(e, Solution().sample(i))
+    def test4(self):
+        i =
+        e =
+        self.assertEqual(e, Solution().sample(i))
+
+
+if __name__ == "__main__":
+    unittest.main()
